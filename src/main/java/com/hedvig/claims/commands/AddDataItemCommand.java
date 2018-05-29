f3bb4b55a70de8b18a86a50791844dcf1d3ed9d6
meletios <meletios@hedvig.com>
2018-05-29 10:06:20 +0200

    fix bug with the claim events

--- src/main/java/com/hedvig/claims/commands/AddDataItemCommand.java
+++ src/main/java/com/hedvig/claims/commands/AddDataItemCommand.java
@@ -14,34 +14,34 @@ import java.time.LocalDateTime;
 @Value
 public class AddDataItemCommand {
 
-	private static Logger log = LoggerFactory.getLogger(AddDataItemCommand.class);
-	
-	public String id;
-	
-	@TargetAggregateIdentifier
-	public String claimID;
-	public LocalDateTime date;
-	public String userId;
-	
-	public DataType type;
-	public String name;
-	public String title;
-	public Boolean recieved;
-	public String value;
+    private static Logger log = LoggerFactory.getLogger(AddDataItemCommand.class);
+
+    public String id;
+
+    @TargetAggregateIdentifier
+    public String claimID;
+    public LocalDateTime date;
+    public String userId;
+
+    public DataType type;
+    public String name;
+    public String title;
+    public Boolean received;
+    public String value;
 
     public AddDataItemCommand(String id,String claimID, LocalDateTime date, String userId, 
-    		DataType type, String name, String title, Boolean recieved, String value) {
+            DataType type, String name, String title, Boolean received, String value) {
         log.info("InitiateClaimCommand");
-    	this.id = id;
-    	this.claimID = claimID;
-    	this.date = date;
-    	this.userId = userId;
-    	
-    	this.type = type;
-    	this.name = name;
-    	this.title = title;
-    	this.recieved = recieved;
-    	this.value = value;
+        this.id = id;
+        this.claimID = claimID;
+        this.date = date;
+        this.userId = userId;
+
+        this.type = type;
+        this.name = name;
+        this.title = title;
+        this.received = received;
+        this.value = value;
         log.info(this.toString());
     } 
 }
