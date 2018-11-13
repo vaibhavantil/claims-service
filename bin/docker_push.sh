0d4b53860b0afe49278cf9c1a2a3573267da9933
Meletios Mazarakiotis <39048374+meletios@users.noreply.github.com>
2018-11-13 10:43:09 +0100

    change docker_push

--- bin/docker_push.sh
+++ bin/docker_push.sh
@@ -8,14 +8,14 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     # Change it accordingly to your docker repo
     pip install --user awscli
     export PATH=$PATH:$HOME/.local/bin
-    eval $(aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION)
+    eval $(aws ecr get-login --no-include-email  --region $AWS_DEFAULT_REGION)
 
     # Build and push
     docker build -t $IMAGE_NAME .
     echo "Pushing $IMAGE_NAME:latest"
-    docker tag $IMAGE_NAME:latest $REMOTE_IMAGE_URL:$TRAVIS_BUILD_NUMBER
-    docker push $REMOTE_IMAGE_URL:$TRAVIS_BUILD_NUMBER
-    echo "Pushed $IMAGE_NAME:$TRAVIS_BUILD_NUMBER"
+    docker tag $IMAGE_NAME:latest $REMOTE_IMAGE_URL:${TRAVIS_COMMIT}
+    docker push $REMOTE_IMAGE_URL:${TRAVIS_COMMIT}
+    echo "Pushed $IMAGE_NAME:${TRAVIS_COMMIT}"
   else
     echo "Skipping deploy because branch is not 'master'"
   fi
