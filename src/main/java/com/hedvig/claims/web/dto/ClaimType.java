f4bec80de3e2c668d26ef485f0b4f1ff51b874d9
Alex Belyaev <alex.bel.izh@gmail.com>
2018-11-13 17:42:53 +0400

    Added 'archive' flag for claim types

--- src/main/java/com/hedvig/claims/web/dto/ClaimType.java
+++ src/main/java/com/hedvig/claims/web/dto/ClaimType.java
@@ -8,10 +8,12 @@ public class ClaimType {
   public String title;
   public ArrayList<ClaimDataType> requiredData = new ArrayList<ClaimDataType>();
   public ArrayList<ClaimDataType> optionalData = new ArrayList<ClaimDataType>();
+  public boolean archive;
 
-  public ClaimType(String name, String title) {
+  public ClaimType(String name, String title, boolean archive) {
     this.name = name;
     this.title = title;
+    this.archive = archive;
   }
 
   public void addRequiredData(ClaimDataType c) {
