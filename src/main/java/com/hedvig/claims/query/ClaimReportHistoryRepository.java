c1fa22ab780b926a5eee8dd594315f64338ee5f9
meletios <meletios@hedvig.com>
2019-04-02 17:57:24 +0200

    add method to fetch claimHistoryEntities for a specific period

--- src/main/java/com/hedvig/claims/query/ClaimReportHistoryRepository.java
+++ src/main/java/com/hedvig/claims/query/ClaimReportHistoryRepository.java
@@ -1,10 +1,16 @@
 package com.hedvig.claims.query;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
 public interface ClaimReportHistoryRepository extends JpaRepository<ClaimReportHistoryEntity, UUID> {
   List<ClaimReportHistoryEntity> findByClaimId(String claimId);
+
+  @Query("from ClaimReportHistoryEntity where timeOfKnowledge >= :from AND timeOfKnowledge <= :to")
+  List<ClaimReportHistoryEntity> findByTimeOfKnowledge(Instant from, Instant to);
 }
