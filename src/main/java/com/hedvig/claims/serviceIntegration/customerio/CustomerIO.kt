0bf7b71cdb2d5b51a451109829bf966c7efef80a
vonElfvin <elvin.granat@gmail.com>
2019-11-06 18:44:47 +0100

    Add logging to customerio

--- src/main/java/com/hedvig/claims/serviceIntegration/customerio/CustomerIO.kt
+++ src/main/java/com/hedvig/claims/serviceIntegration/customerio/CustomerIO.kt
@@ -1,6 +1,7 @@
 package com.hedvig.claims.serviceIntegration.customerio
 
 import com.hedvig.claims.aggregates.ClaimsAggregate
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.context.annotation.Profile
@@ -13,10 +14,16 @@ import org.springframework.stereotype.Component
 class CustomerIO(
     private val customerIOClient: CustomerIOClient
 ){
+    val logger = LoggerFactory.getLogger(CustomerIO::class.java)
+
     fun setClaimStatus(userId: String, claimStatus: ClaimsAggregate.ClaimStates) {
         val traits = mapOf(
             "latest_claim_status" to claimStatus.name
         )
-        this.customerIOClient.put(userId, traits)
+        try {
+            this.customerIOClient.put(userId, traits)
+        } catch (exception: Exception) {
+            logger.error("Could not set \"latest_claim_status\" for $userId to $claimStatus in customer.io (exception=$exception)")
+        }
     }
 }
