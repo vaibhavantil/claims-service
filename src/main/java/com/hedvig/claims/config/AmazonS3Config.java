059be1854fe02d65faa1e741735331430b84f919
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2019-12-05 13:40:10 +0100

    add logging to s3 config

--- src/main/java/com/hedvig/claims/config/AmazonS3Config.java
+++ src/main/java/com/hedvig/claims/config/AmazonS3Config.java
@@ -5,14 +5,17 @@ import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 public class AmazonS3Config {
 
   @Bean
   AmazonS3 s3Client(AWSCredentialsProvider credentialsProvider) {
+    log.info("creating amazon s3Client bean");
     return AmazonS3Client.builder().withCredentials(credentialsProvider)
     .withRegion(Regions.EU_CENTRAL_1).build();
   }
