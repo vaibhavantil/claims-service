32e02fbb2de176ee8a358e49237b2c98a9592fda
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2019-12-05 13:55:43 +0100

    add different profiles

--- src/main/java/com/hedvig/claims/config/AmazonS3Config.java
+++ src/main/java/com/hedvig/claims/config/AmazonS3Config.java
@@ -8,16 +8,30 @@ import com.amazonaws.services.s3.AmazonS3Client;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Slf4j
 @Configuration
 public class AmazonS3Config {
 
   @Bean
+  @Profile("development")
   AmazonS3 s3Client(AWSCredentialsProvider credentialsProvider) {
     log.info("creating amazon s3Client bean");
     return AmazonS3Client.builder().withCredentials(credentialsProvider)
     .withRegion(Regions.EU_CENTRAL_1).build();
+  }
+
+  @Bean
+  @Profile("staging")
+  AmazonS3 s3ClientStaging(AWSCredentialsProvider credentialsProvider) {
+    return AmazonS3Client.builder().withCredentials(credentialsProvider).build();
+  }
+
+  @Bean
+  @Profile("production")
+  AmazonS3 s3ClientProd(AWSCredentialsProvider credentialsProvider) {
+    return AmazonS3Client.builder().withCredentials(credentialsProvider).build();
   }
 
   @Bean
