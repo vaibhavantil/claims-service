59d7ef1f94c2539363bc274d33cc4fb4e4be0a85
meletios <meletios@hedvig.com>
2020-05-28 10:40:35 +0200

    fixing broken build¨

--- src/main/java/com/hedvig/homer/web/HomerController.kt
+++ src/main/java/com/hedvig/homer/web/HomerController.kt
@@ -1,16 +1,17 @@
 package com.hedvig.homer.web
 
-import com.hedvig.homer.handlers.SpeechToTextServiceImpl
 import com.hedvig.homer.handlers.SpeechToTextResult
+import com.hedvig.homer.handlers.SpeechToTextService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 
 @RestController
 class HomerController(
-  val speechToTextService: SpeechToTextServiceImpl
+  val speechToTextService: SpeechToTextService
 ) {
   @RequestMapping(
     path = ["/testSpeechToText"],
@@ -18,7 +19,7 @@ class HomerController(
   )
   fun test(@RequestBody aurioUrl: String): ResponseEntity<*>? {
     val result: SpeechToTextResult =
-      speechToTextService.convertSpeechToText(aurioUrl)
+      speechToTextService.convertSpeechToText(aurioUrl, UUID.randomUUID())
     return ResponseEntity.ok(result.text + result.confidence)
   }
 }
