5e19cda5da35f2b628827b30756b1ca6cfccc1da
michael-duivestein <michael@hedvig.com>
2021-02-01 14:58:12 +0100

    HGV-782: Add new payment types. - Added twp new items to PaymentType: IndemnityCost and Expense. - Added a unified endpoint and DTO for all payments to InternalController. - Created PaymentRequestRouter to create the correct command for each payment type. - For new payment types, added new commands and added new handlers to ClaimsAggregate. - For new payment types, added new events and added new listeners to ClaimsEventListener. - Removed unused field from PaymentAddedEvent. - Updated readme.

---
+++ src/main/java/com/hedvig/claims/commands/AddExpensePaymentCommand.kt
@@ -0,0 +1,16 @@
+package com.hedvig.claims.commands
+
+import org.axonframework.commandhandling.TargetAggregateIdentifier
+import java.time.LocalDateTime
+import javax.money.MonetaryAmount
+
+data class AddExpensePaymentCommand(
+    val id: String,
+    @TargetAggregateIdentifier var claimID: String,
+    var date: LocalDateTime,
+    var amount: MonetaryAmount,
+    var deductible: MonetaryAmount,
+    var note: String?,
+    var exGratia: Boolean,
+    var handlerReference: String?
+)
--- src/main/java/com/hedvig/claims/events/PaymentAddedEvent.java
+++ src/main/java/com/hedvig/claims/events/PaymentAddedEvent.java
@@ -17,7 +17,6 @@ public class PaymentAddedEvent {
   Double amount;
   Double deductible;
   String note;
-  LocalDateTime payoutDate;
   Boolean exGratia;
   String handlerReference;
 }
