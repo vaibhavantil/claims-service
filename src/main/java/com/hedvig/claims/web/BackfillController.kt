8e20a9fbedc7947f94f8e3235d7c68fcfb69f40a
meletios <meletios@hedvig.com>
2021-02-19 10:32:26 +0100

    add test endpoint

--- src/main/java/com/hedvig/claims/web/BackfillController.kt
+++ src/main/java/com/hedvig/claims/web/BackfillController.kt
@@ -2,9 +2,11 @@ package com.hedvig.claims.web
 
 import com.hedvig.claims.commands.TranscribeAudioCommand
 import com.hedvig.claims.query.ClaimsRepository
+import com.hedvig.claims.serviceIntegration.predictor.Predictor
 import com.hedvig.homer.SpeechToTextService
 import org.axonframework.commandhandling.gateway.CommandGateway
 import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -14,17 +16,25 @@ import org.springframework.web.bind.annotation.RestController
 class BackfillController(
     private val claimsRepository: ClaimsRepository,
     private val commandGateway: CommandGateway,
-    private val speechToTextService: SpeechToTextService
+    private val speechToTextService: SpeechToTextService,
+    private val predictor: Predictor
 ) {
     @PostMapping("/audioTranscription")
-    fun backfillAudioTranscription(){
+    fun backfillAudioTranscription() {
         val list = claimsRepository.findAllByTranscriptionsIsNull()
         list.forEach {
             try {
                 log.info("Backfilling audio for claim ${it.id} -  Started")
                 val result = speechToTextService.convertSpeechToText(it.audioURL, it.id)
                 if (result.text.isNotBlank() && result.languageCode.isNotBlank() && result.confidence != 0f) {
-                    commandGateway.send<Void>(TranscribeAudioCommand(it.id, result.text, result.confidence, result.languageCode))
+                    commandGateway.send<Void>(
+                        TranscribeAudioCommand(
+                            it.id,
+                            result.text,
+                            result.confidence,
+                            result.languageCode
+                        )
+                    )
                 }
                 log.info("Backfilling audio for claim ${it.id}  -  Ended")
             } catch (e: Exception) {
@@ -33,7 +43,12 @@ class BackfillController(
         }
     }
 
-    companion object{
+    @PostMapping("/test")
+    fun testPredictor(): ResponseEntity<Boolean> {
+        return ResponseEntity.ok(predictor.predictIfItsAccidentClaim("Jag tappade min iPhone"))
+    }
+
+    companion object {
         val log = LoggerFactory.getLogger(BackfillController::class.java)!!
     }
 }
